3d52c9af68441eb357f7b2642354d7b6ffe023f9
mazinhorj <adm.osmarsilva@gmail.com>
2023-05-29 14:08:44 -0300

    configs

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from "fastify";
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return '?Hello, RESTful World!'
+})
+
+const theAuthor: string = "Osmar Menezes da Silva (Mazinho)";
+app
+  .listen({
+    port: 3333,
+    host: "0.0.0.0",
+  })
+  .then(() => {
+    console.log(
+      `-> Desenvolvido por ${theAuthor} \n-> ✅ Servidor rodando em http://localhost:3333`
+    );
+  });
