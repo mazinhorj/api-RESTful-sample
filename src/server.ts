eeb9e43aa2364311652de0e8f64644582ed46317
mazinhorj <adm.osmarsilva@gmail.com>
2023-05-29 16:11:07 -0300

    eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,14 +6,14 @@ app.get('/hello', () => {
   return '?Hello, RESTful World!'
 })
 
-const theAuthor: string = "Osmar Menezes da Silva (Mazinho)";
+const theAuthor: string = 'Osmar Menezes da Silva (Mazinho)'
 app
   .listen({
     port: 3333,
-    host: "0.0.0.0",
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log(
-      `-> Desenvolvido por ${theAuthor} \n-> ✅ Servidor rodando em http://localhost:3333`
-    );
-  });
+      `-> Desenvolvido por ${theAuthor} \n-> ✅ Servidor rodando em http://localhost:3333`,
+    )
+  })
