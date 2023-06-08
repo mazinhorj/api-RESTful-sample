93a77f38aa2b8bce13e9959ac512c1b73f848f7d
mazinhorj <65859903+mazinhorj@users.noreply.github.com>
2023-06-08 15:42:54 -0300

    production configs

--- src/dbase.ts
+++ src/dbase.ts
@@ -3,10 +3,13 @@ import { Knex, knex as setKnex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL, // './db/tmp/app.db', // pode ser .sqlite
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL, // './db/tmp/app.db', // pode ser .sqlite
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- src/env/index.ts
+++ src/env/index.ts
@@ -11,9 +11,10 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['production', 'test', 'development']).default('production'), //
+  NODE_ENV: z.enum(['production', 'test', 'development']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
